10734366e236c5eb82bd57682e38616bd510187a
Elena Kotlyarova <elenakotlyrova@gmail.com>
2022-11-08 17:23:29 +0300

    feat: define the app model

--- repeatit/repeatit_app/models.py
+++ repeatit/repeatit_app/models.py
@@ -1,3 +1,26 @@
+"""
+The model defines how the db tables looks.
+"""
+
 from django.db import models
 
-# Create your models here.
+# A box means a repetition interval
+NUM_BOXES = 3
+BOXES = range(1, NUM_BOXES + 1)
+
+
+class Flashcard(models.Model):
+    """
+    A flashcard has a question on one side and an answer on the other.
+    """
+
+    question = models.CharField(max_length=100)
+    answer = models.CharField(max_length=100)
+    box = models.IntegerField(
+        choices=zip(BOXES, BOXES),
+        default=BOXES[0],
+    )
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.question)
