42e5fffac66fc2e62f678e7376f6d74e6dd8de32
Elena Kotlyarova <elenakotlyrova@gmail.com>
2022-11-21 17:55:32 +0300

    feat: add user authentication

--- repeatit/repeatit_app/urls.py
+++ repeatit/repeatit_app/urls.py
@@ -3,6 +3,7 @@ URL configuration to map URL paths to the application views
 """
 
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from .views import FlashcardListView, FlashcardCreateView, FlashcardUpdateView, BoxView
 
 
@@ -11,4 +12,5 @@ urlpatterns = [
     path("new", FlashcardCreateView.as_view(), name="flashcard-create"),
     path("edit/<int:pk>", FlashcardUpdateView.as_view(), name="flashcard-update"),
     path("box/<int:box_num>", BoxView.as_view(), name="box"),
+    path("accounts/login", auth_views.LoginView.as_view(template_name='repeatit_app/login.html'))
 ]
